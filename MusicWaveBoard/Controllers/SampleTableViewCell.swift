7769ba41bd9ad2a7a62558582205a76c0bfe978a
Vitaliy Gribko <vitaliy69@gmail.com>
2023-03-26 17:43:23 +0300

    Added sample view design

---
+++ MusicWaveBoard/Controllers/SampleTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  SampleCell.swift
+//  MusicWaveBoard
+//
+//  Created by Vitaliy Gribko on 26.03.2023.
+//
+
+import UIKit
+
+class SampleTableViewCell: UITableViewCell {
+
+    
+    @IBOutlet weak var sampleImageView: UIImageView!
+    @IBOutlet weak var sampleLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        let verticalSpace: CGFloat = 5.0
+        self.contentView.frame = self.contentView.frame.inset(by: UIEdgeInsets(top: verticalSpace, left: 0, bottom: verticalSpace, right: 0))
+    }
+}
